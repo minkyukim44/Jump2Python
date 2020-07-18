5db9c6fc49e9bbb94b4470de74071b77cffdb868
minkyukim44 <mkimintruth@gmail.com>
2020-07-18 23:41:42 +0900

    Practice Strings

--- practice_2_2.py
+++ practice_2_2.py
@@ -70,3 +70,42 @@ f'{y:0.4f}'
 f'{y:10.4f}'
 
 f'{{ and }}'
+
+f'{"python":!^12}'
+"{0:!^12}".format("python")
+
+## About character
+
+a = "hobby"
+a.count('b')        # count 'b' in 'hobby'
+
+a = "Python is the best choice"
+a.find('b')         # find the location where 'b' is
+a.find('k')         # the letter 'k' does not exist (-1)
+
+a = "Life is too short"
+a.index('t')        # find the location where 't' is
+a.index('k')        # the letter 'k' does not exist (error)
+
+",".join('abcd')    # insert "," between each letter
+",".join(['a','b','c','d'])
+
+a = "hi"
+a.upper()           # capitalize letters
+
+a = "HI"
+a.lower()           # de-capitalize letters
+
+a = " hi "
+a.lstrip()          # remove spaces on the left side
+a.rstrip()          # remove spaces on the right side
+a.strip()           # remove spaces on the both sides
+
+a = "Life is too short"
+a.replace("Life", "Your leg")       # replace phrases
+a.split()                           # divide characters by spaces
+
+b = "a:b:c:d"
+b.split(':')                        # divide characters by ':'
+
+
