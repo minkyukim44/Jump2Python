30eff61945a25f8a6abb445871667280fc62f506
minkyukim44 <mkimintruth@gmail.com>
2020-07-20 23:36:15 +0900

    Practice Tuples

---
+++ practice_2_4.py
@@ -0,0 +1,25 @@
+## Practice Tuple
+
+t1 = (1,)
+t4 = 1,2,3
+
+t1 = (1,2,'a','b')
+del t1[0]       # cannot delete an element in a tuple
+t1[0] = 'c'     # cannot change an element in a tuple
+t1[0]
+t1[3]
+t1[1:]
+
+t2 = (3,4)
+t1 + t2
+t2*3
+len(t1)
+
+t0 = (1,2,3)
+t00 = (4,)
+tadd = t0 + t00 
+tadd
+
+
+
+
