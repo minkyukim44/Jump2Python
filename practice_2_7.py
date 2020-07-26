4f40d5e5fe34de6d17a7faed2d758752ad21a6cc
minkyukim44 <mkimintruth@gmail.com>
2020-07-26 22:18:21 +0900

    Practice Chapter 2 / boolean

---
+++ practice_2_7.py
@@ -0,0 +1,26 @@
+## Boolean data
+
+a = True
+b = False
+type(a)         # check a type of data
+type(b)
+
+1 == 1
+2 > 1
+2 < 1
+
+a = [1,2,3,4]
+while a:
+    a.pop()     # take out each element from the most right side
+
+if []:
+    print("true1")
+else:
+    print("false1")
+
+bool('python')
+bool('')
+bool([1,2,3])
+bool([])
+bool(0)
+bool(3)  
