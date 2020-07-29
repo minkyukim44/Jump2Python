ce14cdba7b4467f4b0e9c6adf7bb1c01763d4b6a
minkyukim44 <mkimintruth@gmail.com>
2020-07-29 23:36:51 +0900

    Practice Chapter 2

---
+++ practice_2_8.py
@@ -0,0 +1,41 @@
+## Variables
+
+a = [1,2,3]
+id(a)               # check memory address of a
+
+b = a
+b
+id(a)
+id(b)               # a and b are identical
+a is b
+a[1] = 4
+a
+b 
+
+a = [1,2,3]
+b = a[:]            # slicing
+a[1] = 4
+a
+b                   # a and b are different
+
+from copy import copy       # use a function, 'copy'
+a = [1,2,3]
+b = copy(a)
+a 
+b
+b is a              # a and b are different
+
+a, b = ('python','life')
+(a,b) = 'python','life'
+[a,b] = ['python','life']
+a = b = 'python'
+
+a = 3
+b = 5
+a, b = b, a         # exchange
+a
+b
+
+a = [1,2,3]
+b = [1,2,3]
+a is b              # different memory addresses
---
+++ practice_2_Q.py
@@ -0,0 +1,16 @@
+# Q1
+a = [80, 75, 55]
+sum(a)/3
+
+# Q2
+13 % 2
+
+# Q3
+pin = "881120-1068234"
+yyyymmdd = pin[:6]
+num = pin[7:]
+print(yyyymmdd)
+print(num)
+
+# Q4
+print(pin[7])
