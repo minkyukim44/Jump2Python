e69c6156fddd30846d4e9e81a4006f252f30b9ee
minkyukim44 <mkimintruth@gmail.com>
2020-07-30 23:06:15 +0900

    Practice Chapter 2 / questions

--- practice_2_Q.py
+++ practice_2_Q.py
@@ -14,3 +14,41 @@ print(num)
 
 # Q4
 print(pin[7])
+
+# Q5
+a = "a:b:c:d"
+b = a.replace(":", "#")
+print(b)
+
+# Q6
+a = [1, 3, 5, 4, 2]
+a.sort()
+a.reverse()
+print(a)
+
+# Q7
+a = ['Life', 'is', 'too', 'short']
+result = " ".join(a)
+print(result)
+
+# Q8
+a = (1, 2, 3)
+a = a + (4,)
+print(a)
+
+# Q10
+a = {'A': 90, 'B': 80, 'C': 70}
+result = a.pop('B')
+print(a)
+print(result)
+
+# Q11
+a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
+aSet = set(a)
+b = list(aSet)
+print(b)
+
+# Q12
+a = b = [1, 2, 3]
+a[1] = 4
+print(b)
