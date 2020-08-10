1e1cca66673d4e9399badc2410e7b6700c84e4b9
minkyukim44 <mkimintruth@gmail.com>
2020-08-10 23:31:37 +0900

    Practice Chapter 4 / various inputs and outputs

---
+++ Chapter 4/practice_4_2.py
@@ -0,0 +1,16 @@
+# 'input()' function
+a = input()
+
+number = input("Type a number:")
+
+# 'print()' function
+a =123
+print(a)
+
+print("life" "is" "too short")
+print("life"+"is"+"too short")
+print("life","is","too short")
+
+for i in range(10):
+    print(i, end=' ')   # put all outputs on a line
+    
